d4c7cc775a91bff706559965b99761cf8a4b4f45
Thai Do <thai.hl.do@gmail.com>
2020-09-19 15:58:47 -0700

    Created routes for users

--- routes/users-router.js
+++ routes/users-router.js
@@ -7,19 +7,50 @@
 
 const express = require('express');
 const router  = express.Router();
+const { getUsers, getUserById } = require('../db/helperquery/users-queries');
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+// GET /users/
+router.get('/', (req, res) => {
+  getUsers()
+    .then((users) => {
+      res.json({ users });
+    });
+});
+
+// create a fake route to test to make sure its working
+router.get('/test', (req, res) => {
+
+      res.json({ result: true });
+});
+
+// GET /users/:id
+router.get('/:id', (req, res) => {
+  getUserById(req.params.id)
+    .then((user) => {
+      res.json({ user });
+    })
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+// module.exports = (db) => {
+//   router.get("/", (req, res) => {
+//     db.query(`SELECT * FROM users;`)
+//       .then(data => {
+//         const users = data.rows;
+//         res.json({ users });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+//   return router;
+// };
