1a2a75d69ba82386cb05d244b9a2213fa2b01e5b
Thai Do <thai.hl.do@gmail.com>
2020-09-18 19:50:33 -0700

    Added names to table for data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+INSERT INTO users (name)
+VALUES ('Alice Wonderland'),
+('Bob Thortan'),
+('Thor Odinhall'),
+('Ryhs Brooks'),
+('Ryand Orion');
