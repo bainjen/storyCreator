54d14a4abce1ba0e3e5db3104d4e9f7beca76963
Thai Do <thai.hl.do@gmail.com>
2020-09-18 20:46:46 -0700

    Created tables and seeds for upVotes

---
+++ db/schema/04_upVotes.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS upVotes CASCADE;
+
+CREATE TABLE upVotes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  contribution_id INTEGER REFERENCES contributions(id) NOT NULL,
+  name_id INTEGER REFERENCES users(id) NOT NULL
+);
---
+++ db/seeds/04_upVotes.sql
@@ -0,0 +1,11 @@
+INSERT INTO upVotes (contribution_id, name_id)
+VALUES (2, 3);
+
+INSERT INTO upVotes (contribution_id, name_id)
+VALUES(1, 2);
+
+INSERT INTO upVotes (contribution_id, name_id)
+VALUES (1, 2);
+
+INSERT INTO upVotes (contribution_id, name_id)
+VALUES (3, 3);
