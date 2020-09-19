4b8f44eced98b14d992352345177736e32ef61aa
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-09-19 17:54:11 +0000

    save before pulling from master to get queries data

--- routes/login-router.js
+++ routes/login-router.js
@@ -1,5 +1,53 @@
-// do this instead
-app.get('/login/:id', (req, res) => {
-  req.session.user_id = req.params.id;
-  res.redirect('/');
+const express = require('express');
+const router = express.Router();
+
+const login = (db) => {
+  router.get('/', (req, res) => {
+    res.render('login');
+    console.log("hello where am I ")
+  })
+  return router;
+}
+
+module.exports = login;
+
+
+router.get("/", (req, res) => {
+  db.query(`SELECT * FROM users;`)
+    .then(data => {
+      const users = data.rows;
+      res.json({ users });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
 });
+return router;
+
+
+
+// const postRouter = (db) => {
+//   // GET /posts/
+//   router.get('/', (req, res) => {
+//     const query = 'SELECT * FROM posts;';
+//     db.query(query)
+//       .then((response) => {
+//         res.json({ posts: response.rows });
+//       });
+//   });
+
+//   // GET /posts/:id
+//   router.get('/:id', (req, res) => {
+//     const query = 'SELECT * FROM posts WHERE id = $1;';
+//     db.query(query, [req.params.id])
+//       .then((response) => {
+//         res.json({ post: response.rows[0] });
+//       });
+//   });
+
+//   return router;
+// };
+
+// module.exports = postRouter;
