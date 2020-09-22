156dcc72f7dc8a21194a2f6bc6a00777c0a6f4f2
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-09-22 20:11:11 +0000

    tied author name to ejs rendering by altering the query code to join the users table with name

--- db/helperquery/story-query.js
+++ db/helperquery/story-query.js
@@ -29,7 +29,7 @@ const browseSelectStories = (id) => {
 
 // to grab story/:id to read (GET)
 const getStoryById = (id) => {
-  return db.query("SELECT * FROM stories WHERE id = $1;", [id])
+  return db.query("SELECT stories.*, users.name FROM stories JOIN users ON users.id = stories.name_id WHERE stories.id = $1;", [id])
   .then((response) => {
     return response.rows[0];
   })
