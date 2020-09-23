18a06e7d71995f8a31187b3924360467e54ffd88
Thai Do <thai.hl.do@gmail.com>
2020-09-23 14:13:47 -0700

    Added a Unique attribute

--- db/schema/04_upVotes.sql
+++ db/schema/04_upVotes.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS upVotes CASCADE;
 CREATE TABLE upVotes (
   id SERIAL PRIMARY KEY NOT NULL,
   contribution_id INTEGER REFERENCES contributions(id) NOT NULL,
-  name_id INTEGER REFERENCES users(id) NOT NULL
+  name_id INTEGER REFERENCES users(id) NOT NULL,
+  UNIQUE (contribution_id, name_id)
 );
