3889e0d3bf3fe37b74b9864bb54764f26e4b3a50
Thai Do <thai.hl.do@gmail.com>
2020-09-23 14:14:59 -0700

    Added routes

--- routes/contributions-router.js
+++ routes/contributions-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getStoryContributions, addContribution, getCompletedStory, incompleteStory } = require('../db/helperquery/contribution-query');
+const { getUpVotes, getStoryContributions, addContribution, getCompletedStory, incompleteStory, addUpVote } = require('../db/helperquery/contribution-query');
 
 //need to test this route
 router.get('/:id', (req, res) => {
@@ -11,4 +11,23 @@ router.get('/:id', (req, res) => {
     .catch((err) => console.log("Error for getStoryContributions", err));
 });
 
+router.post('/:id/upVotes', (req, res) => {
+  console.log("SESSION ID", req.session.userid)
+  const userid = req.session.userid;
+  addUpVote(req.params.id, userid)
+    .then(({rows}) => {
+      // INSERT
+      //add a query to count upvotes
+      console.log("INSERTED ROW", rows[0])
+      return getUpVotes(req.params.id);
+      // res.send('All good')
+    })
+    .then(({rows}) => {
+      console.log("COUNT ROWS!!", rows[0])
+      res.json({...rows[0]})
+    })
+    .catch(err => console.log("Error with addupvote", err))
+})
+
+module.exports = router;
 
