7349e85b21b40cd01c6585d9fa36fdca3aa7deee
Thai Do <thai.hl.do@gmail.com>
2020-09-23 14:57:43 -0700

    changed attribute vaules for database and seeds

--- db/seeds/03_contributions.sql
+++ db/seeds/03_contributions.sql
@@ -1,8 +1,8 @@
 INSERT INTO contributions (story_id, name_id, text_addon, accepted_at)
-VALUES (1, 1, 'Maybe broccoli should just be broccolie', '2020-02-20T09:30:00.000Z');
+VALUES (1, 1, 'Maybe broccoli should just be broccolie', NULL);
 
 INSERT INTO contributions (story_id, name_id, text_addon, accepted_at)
-VALUES (3, 2, 'should rabbits fly in the air or swim in the sea', '2020-01-19T08:15:00.000Z');
+VALUES (3, 2, 'should rabbits fly in the air or swim in the sea', NULL);
 
 INSERT INTO contributions (story_id, name_id, text_addon, accepted_at)
-VALUES (2, 3, 'Sometimes when I feel hungry I eat butter BUT...', '2020-02-20T09:30:00.000Z');
+VALUES (2, 3, 'Sometimes when I feel hungry I eat butter BUT...', NULL);
