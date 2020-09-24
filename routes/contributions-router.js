a1c37d28c7be5ccafd1e8da6cb2aa981cdbe51e8
Thai Do <thai.hl.do@gmail.com>
2020-09-24 09:49:37 -0700

    Cleaned up code and added comments to routes

--- routes/contributions-router.js
+++ routes/contributions-router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getUpVotes, getStoryContributions, addContribution, getCompletedStory, incompleteStory, addUpVote } = require('../db/helperquery/contribution-query');
+const { getUpVotes, getStoryContributions, addUpVote } = require('../db/helperquery/contribution-query');
 
-//need to test this route
+// GET Route to /contributions/:id
 router.get('/:id', (req, res) => {
   getStoryContributions(req.params.id)
     .then((contributions) => {
@@ -11,19 +11,14 @@ router.get('/:id', (req, res) => {
     .catch((err) => console.log("Error for getStoryContributions", err));
 });
 
+// POST Route to /contributions/:id/upVotes
 router.post('/:id/upVotes', (req, res) => {
-  console.log("SESSION ID", req.session.userid)
   const userid = req.session.userid;
   addUpVote(req.params.id, userid)
     .then(({rows}) => {
-      // INSERT
-      //add a query to count upvotes
-      console.log("INSERTED ROW", rows[0])
       return getUpVotes(req.params.id);
-      // res.send('All good')
     })
     .then(({rows}) => {
-      console.log("COUNT ROWS!!", rows[0])
       res.json({...rows[0]})
     })
     .catch(err => console.log("Error with addupvote", err))
